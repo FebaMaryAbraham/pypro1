3c964aaba7c591dc5f913919c890c1b819c58b6e
FebaMaryAbraham <96905822+FebaMaryAbraham@users.noreply.github.com>
2021-12-31 13:48:48 +0530

    Add files via upload

---
+++ pg2.py
@@ -0,0 +1,8 @@
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
